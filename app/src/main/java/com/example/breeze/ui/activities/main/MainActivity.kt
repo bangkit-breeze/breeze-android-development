e37d81d3c287125ed398c064954e4e870deb0cef
pierreviences <imaniyazid@gmail.com>
2023-11-27 18:04:51 +0700

    feat: implement splash screen with onboarding check

---
+++ app/src/main/java/com/example/breeze/ui/activities/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.breeze.ui.activities.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.breeze.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/breeze/ui/fragments/splash/SplashFragment.kt
@@ -0,0 +1,45 @@
+package com.example.breeze.ui.fragments.splash
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.breeze.R
+
+class SplashFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (onBoardingIsFinished()){
+                findNavController().navigate(R.id.action_splashFragment_to_loginActivity)
+            }else{
+                findNavController().navigate(R.id.action_splashFragment_to_onBoardingFragment)
+            }
+
+        }, 3000)
+
+
+        val view = inflater.inflate(R.layout.fragment_splash, container, false)
+
+        return view
+
+    }
+    private fun onBoardingIsFinished(): Boolean{
+
+        val sharedPreferences = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean("finished",false)
+    }
+
+
+}
