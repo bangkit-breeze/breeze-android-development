b43e1062d856362e5480f011586723a9a991b5e1
pierreviences <imaniyazid@gmail.com>
2023-11-27 18:06:00 +0700

    feat: implement onboarding fragment with viewpager

---
+++ app/src/main/java/com/example/breeze/ui/fragments/onboarding/OnBoardingFragment.kt
@@ -0,0 +1,46 @@
+package com.example.breeze.ui.fragments.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.breeze.R
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
+
+class OnBoardingFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_on_boarding, container, false)
+
+
+        val fragmentList = arrayListOf<Fragment>(
+            FirstScreen(),
+            SecondScreen(),
+            ThirdScreen()
+        )
+
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
+
+        viewPager.adapter = adapter
+        val indicator = view.findViewById<DotsIndicator>(R.id.dots_indicator)
+
+        indicator.attachTo(viewPager)
+
+        return view
+    }
+
+
+}
