1d18d2e1be96907730b63ea76774faff13949aec
pierreviences <imaniyazid@gmail.com>
2023-11-28 15:49:20 +0700

    feat: Add animation to the login screen

--- app/src/main/java/com/example/breeze/ui/activities/login/LoginActivity.kt
+++ app/src/main/java/com/example/breeze/ui/activities/login/LoginActivity.kt
@@ -1,20 +1,58 @@
 package com.example.breeze.ui.activities.login
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.LifecycleObserver
 import com.example.breeze.R
+import com.example.breeze.databinding.ActivityLoginBinding
+import com.example.breeze.utils.Constants
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        playAnimations()
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
         finishAffinity()
+    }
+
+    private fun playAnimations() {
+        val fadeInDuration = Constants.DURATION_ANIMATION_DELAY
+
+        val titleAnimator = createFadeInAnimator(binding.tvTitle, fadeInDuration)
+        val messageAnimator = createFadeInAnimator(binding.tvTitle2, fadeInDuration)
+        val emailTextAnimator = createFadeInAnimator(binding.tvEmail, fadeInDuration)
+        val emailEditTextAnimator = createFadeInAnimator(binding.emailEditTextLayout, fadeInDuration)
+        val passwordTextAnimator = createFadeInAnimator(binding.tvPassword, fadeInDuration)
+        val passwordEditTextAnimator = createFadeInAnimator(binding.passwordEditTextLayout, fadeInDuration)
+        val loginButtonAnimator = createFadeInAnimator(binding.btnLogin, fadeInDuration)
+        val registerTextAnimator = createFadeInAnimator(binding.textViewToRegister, fadeInDuration)
+
+        AnimatorSet().apply {
+            playSequentially(
+                titleAnimator,
+                messageAnimator,
+                emailTextAnimator,
+                emailEditTextAnimator,
+                passwordTextAnimator,
+                passwordEditTextAnimator,
+                loginButtonAnimator,
+                registerTextAnimator
+            )
+            startDelay = fadeInDuration
+        }.start()
+    }
+
+    private fun createFadeInAnimator(view: View, duration: Long): ObjectAnimator {
+        return ObjectAnimator.ofFloat(view, View.ALPHA, 1f).setDuration(duration)
     }
 }
--- app/src/main/java/com/example/breeze/utils/Constants.kt
+++ app/src/main/java/com/example/breeze/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.example.breeze.utils
 
 object Constants {
     const val SPLASH_SCREEN_DELAY = 3000L
+    const val DURATION_ANIMATION_DELAY = 250L
 }
