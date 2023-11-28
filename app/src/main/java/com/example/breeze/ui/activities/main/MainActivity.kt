0c04477c4485f30a8092091173dc214772c467d8
pierreviences <imaniyazid@gmail.com>
2023-11-28 22:11:56 +0700

    feat(ui): bottom sheet dialog for tracked carbon

--- app/src/main/java/com/example/breeze/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/example/breeze/ui/activities/main/MainActivity.kt
@@ -6,37 +6,43 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.breeze.R
 import com.example.breeze.databinding.ActivityMainBinding
+import com.example.breeze.databinding.BottomDialogTrackedBinding
 import com.example.breeze.ui.fragments.event.EventFragment
 import com.example.breeze.ui.fragments.home.HomeFragment
 import com.example.breeze.ui.fragments.leaderboard.LeaderBoardFragment
 import com.example.breeze.ui.fragments.profile.ProfileFragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private var prevSelectedItemId: Int = R.id.bottom_home
+    private  val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.bottomNavigation.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.bottom_home -> {
                     replaceFragment(HomeFragment())
+                    prevSelectedItemId = R.id.bottom_home
                     true
                 }
                 R.id.bottom_event -> {
                     replaceFragment(EventFragment())
+                    prevSelectedItemId = R.id.bottom_event
                     true
                 }
                 R.id.bottom_add ->{
-                    Toast.makeText(this, "feature not yet", Toast.LENGTH_SHORT).show()
+                    showBottomSheet()
                     true
                 }
                 R.id.bottom_leaderboard -> {
                     replaceFragment(LeaderBoardFragment())
+                    prevSelectedItemId = R.id.bottom_leaderboard
                     true
                 }
                 R.id.bottom_profile -> {
                     replaceFragment(ProfileFragment())
+                    prevSelectedItemId = R.id.bottom_profile
                     true
                 }
                 else -> false
@@ -44,6 +50,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         replaceFragment(HomeFragment())
+    }
+
+    private fun showBottomSheet(){
+        val sheetdialog = BottomSheetDialog(this)
+        val sheetBinding = BottomDialogTrackedBinding.inflate(layoutInflater)
+        sheetdialog.setContentView(sheetBinding.root)
+        sheetdialog.apply {
+            setContentView(sheetBinding.root)
+            setOnDismissListener {
+                binding.bottomNavigation.selectedItemId = prevSelectedItemId
+            }
+            show()
+        }
+
+        sheetBinding.btnFood.setOnClickListener {
+            Toast.makeText(this, "Feature not yed", Toast.LENGTH_SHORT).show()
+        }
+        sheetBinding.btnVechile.setOnClickListener {
+            Toast.makeText(this, "Feature not yed", Toast.LENGTH_SHORT).show()
+        }
+        sheetBinding.btnCancel.setOnClickListener{
+            binding.bottomNavigation.selectedItemId = prevSelectedItemId
+            sheetdialog.dismiss()
+        }
     }
     private fun replaceFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction().replace(R.id.navFragment, fragment).commit()
