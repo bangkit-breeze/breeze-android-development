5e58b80f6bde42c5886fb0afa11d1634d5f3b061
pierreviences <imaniyazid@gmail.com>
2023-11-28 16:41:17 +0700

    style: add register page view

--- app/src/main/java/com/example/breeze/ui/activities/register/RegisterActivity.kt
+++ app/src/main/java/com/example/breeze/ui/activities/register/RegisterActivity.kt
@@ -1,4 +1,4 @@
-package com.example.breeze.ui.activities.login
+package com.example.breeze.ui.activities.register
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
@@ -6,59 +6,53 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
-import androidx.lifecycle.LifecycleObserver
 import com.example.breeze.R
-import com.example.breeze.databinding.ActivityLoginBinding
-import com.example.breeze.ui.activities.main.MainActivity
-import com.example.breeze.ui.activities.register.RegisterActivity
+import com.example.breeze.databinding.ActivityRegisterBinding
+import com.example.breeze.ui.activities.login.LoginActivity
 import com.example.breeze.utils.Constants
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupClickListeners()
         playAnimations()
     }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        finishAffinity()
-    }
-
     private fun setupClickListeners() {
-        binding.tvLoginToRegister.setOnClickListener {
-            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
-        }
-        binding.btnLogin.setOnClickListener {
-            Toast.makeText(this, "asd", Toast.LENGTH_SHORT).show()
+        binding.tvRegisterToLogin.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
         }
     }
-
     private fun playAnimations() {
         val fadeInDuration = Constants.DURATION_ANIMATION_DELAY
         val titleAnimator = createFadeInAnimator(binding.tvTitle, fadeInDuration)
-        val messageAnimator = createFadeInAnimator(binding.tvTitle2, fadeInDuration)
+        val titleAnimator2 = createFadeInAnimator(binding.tvTitle2, fadeInDuration)
+        val nameTextAnimator = createFadeInAnimator(binding.tvName, fadeInDuration)
+        val nameEditTextAnimator = createFadeInAnimator(binding.nameEditTextLayout, fadeInDuration)
         val emailTextAnimator = createFadeInAnimator(binding.tvEmail, fadeInDuration)
         val emailEditTextAnimator = createFadeInAnimator(binding.emailEditTextLayout, fadeInDuration)
         val passwordTextAnimator = createFadeInAnimator(binding.tvPassword, fadeInDuration)
         val passwordEditTextAnimator = createFadeInAnimator(binding.passwordEditTextLayout, fadeInDuration)
-        val loginButtonAnimator = createFadeInAnimator(binding.btnLogin, fadeInDuration)
-        val registerTextAnimator = createFadeInAnimator(binding.textViewToRegister, fadeInDuration)
+        val passwordConfirmTextAnimator = createFadeInAnimator(binding.tvConfirmPassword, fadeInDuration)
+        val passwordConfirmEditTextAnimator = createFadeInAnimator(binding.confirmPasswordEditTextLayout, fadeInDuration)
+        val registerButtonAnimator = createFadeInAnimator(binding.btnRegister, fadeInDuration)
+        val registerTextAnimator = createFadeInAnimator(binding.textViewToLogin, fadeInDuration)
 
         AnimatorSet().apply {
             playSequentially(
                 titleAnimator,
-                messageAnimator,
+                titleAnimator2,
+                nameTextAnimator,
+                nameEditTextAnimator,
                 emailTextAnimator,
                 emailEditTextAnimator,
                 passwordTextAnimator,
                 passwordEditTextAnimator,
-                loginButtonAnimator,
+                passwordConfirmTextAnimator,
+                passwordConfirmEditTextAnimator,
+                registerButtonAnimator,
                 registerTextAnimator
             )
             startDelay = fadeInDuration
