b85982d539895cc24d3b607c24f42004a206496e
pierreviences <imaniyazid@gmail.com>
2023-12-02 18:40:36 +0700

    style(ui): Add Leaderboard fragment

--- app/src/main/java/com/example/breeze/ui/fragments/leaderboard/LeaderBoardFragment.kt
+++ app/src/main/java/com/example/breeze/ui/fragments/leaderboard/LeaderBoardFragment.kt
@@ -5,55 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.breeze.R
+import com.example.breeze.databinding.FragmentEventBinding
+import com.example.breeze.databinding.FragmentLeaderBoardBinding
+import com.example.breeze.ui.adapter.EventPagerAdapter
+import com.example.breeze.ui.fragments.event.screen.ActiveEventFragment
+import com.example.breeze.ui.fragments.event.screen.ExploreEventFragment
+import com.example.breeze.ui.fragments.event.screen.FinishedEventFragment
+import com.example.breeze.ui.fragments.leaderboard.screen.AllLeaderBoardFragment
+import com.example.breeze.ui.fragments.leaderboard.screen.WeekLeaderBoardFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-/**
- * A simple [Fragment] subclass.
- * Use the [LeaderBoardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+//
 class LeaderBoardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentLeaderBoardBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leader_board, container, false)
+        _binding =  FragmentLeaderBoardBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LeaderBoardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LeaderBoardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewPagerAndTabLayout()
+    }
+
+    private fun setupViewPagerAndTabLayout() {
+        val viewPager: ViewPager2 = binding?.viewPager ?: return
+        val adapter = EventPagerAdapter(this)
+        adapter.addFragment(AllLeaderBoardFragment())
+        adapter.addFragment(WeekLeaderBoardFragment())
+        viewPager.adapter = adapter
+
+        val tabLayout: TabLayout = binding?.tabs ?: return
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "All Time"
+                1 -> "Weekly"
+                else -> ""
             }
+        }.attach()
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
