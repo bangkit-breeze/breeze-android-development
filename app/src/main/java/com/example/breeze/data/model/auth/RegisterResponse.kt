d1ee8528b26bd58bc550c504b5c03bfabaa5c429
pierreviences <imaniyazid@gmail.com>
2023-12-07 18:59:07 +0700

    feat: Add RegisterResponse models with Gson annotations

---
+++ app/src/main/java/com/example/breeze/data/model/auth/RegisterResponse.kt
@@ -0,0 +1,30 @@
+package com.example.breeze.data.model.auth
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+data class RegisterResponse(
+
+	@field:SerializedName("data")
+	val registerResult: RegisterResult,
+
+	@field:SerializedName("success")
+	val success: Boolean? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
+
+@Parcelize
+data class RegisterResult(
+
+	@field:SerializedName("fullName")
+	val fullName: String? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("email")
+	val email: String? = null
+): Parcelable
