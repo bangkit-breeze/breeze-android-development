9bac543a8c378ffeb74929a8cda6bbc1bbbc24ee
pierreviences <imaniyazid@gmail.com>
2023-12-07 19:21:41 +0700

    add: Add DataStore Preferences dependency version 1.0.0

---
+++ app/src/main/java/com/example/breeze/data/local/datastore/UserPreferences.kt
@@ -0,0 +1,4 @@
+package com.example.breeze.data.local.datastore
+
+class UserPreferences {
+}
---
+++ app/src/main/java/com/example/breeze/data/repository/UserRepository.kt
@@ -0,0 +1,15 @@
+package com.example.breeze.data.repository
+
+import android.app.Application
+import com.example.breeze.api.ApiService
+
+class UserRepository private constructor(
+    private var apiService: ApiService,
+    private val application: Application,
+) {
+
+
+
+
+
+}
