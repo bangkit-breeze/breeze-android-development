c971b88f5bb185b1c55d4b19d3266b0b9ce626e4
pierreviences <imaniyazid@gmail.com>
2023-12-07 19:27:55 +0700

    feat: add sealed class 'Result' for handling outcomes

--- app/src/main/java/com/example/breeze/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/breeze/data/repository/UserRepository.kt
@@ -2,14 +2,28 @@ package com.example.breeze.data.repository
 
 import android.app.Application
 import com.example.breeze.api.ApiService
+import com.example.breeze.data.local.datastore.UserPreferences
 
 class UserRepository private constructor(
     private var apiService: ApiService,
     private val application: Application,
+    private val userPref: UserPreferences
 ) {
 
 
 
 
+    companion object {
+        @Volatile
+        private var instance: UserRepository? = null
+        fun getInstance(
+            apiService: ApiService,
+            application: Application,
+            pref: UserPreferences
+        ): UserRepository =
+            instance ?: synchronized(this) {
+                instance ?: UserRepository(apiService, application, pref)
+            }.also { instance = it }
+    }
 
 }
