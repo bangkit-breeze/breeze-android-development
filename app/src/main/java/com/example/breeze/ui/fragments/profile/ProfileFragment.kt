7034b7b4af473d72ad54728b9c60db6702b49e14
pierreviences <imaniyazid@gmail.com>
2023-12-08 18:56:35 +0700

    feat: add logout confirmation dialog

--- app/src/main/java/com/example/breeze/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/breeze/ui/fragments/profile/ProfileFragment.kt
@@ -5,22 +5,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.breeze.R
 import com.example.breeze.databinding.FragmentHomeBinding
 import com.example.breeze.databinding.FragmentProfileBinding
 import com.example.breeze.ui.activities.details.carbon.DetailCarbonActivity
+import com.example.breeze.ui.activities.login.LoginActivity
+import com.example.breeze.ui.activities.login.LoginViewModel
 import com.example.breeze.ui.activities.profile.DetailProfileActivity
 import com.example.breeze.ui.activities.profile.EditProfileActivity
 import com.example.breeze.ui.activities.profile.exchange.ExchangeTokenActivity
 import com.example.breeze.ui.activities.profile.help.HelpCenterActivity
+import com.example.breeze.ui.factory.AuthViewModelFactory
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-
+    private val  loginViewModel: LoginViewModel by viewModels {
+        AuthViewModelFactory.getInstance(requireActivity().application)
+    }
+    private lateinit var alertDialog: AlertDialog.Builder
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,18 +49,21 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                 val intent = Intent(activity, DetailProfileActivity::class.java)
                 startActivity(intent)
             }
-            binding.btnHelpcenter.setOnClickListener {
+            btnHelpcenter.setOnClickListener {
                 val intent = Intent(activity, HelpCenterActivity::class.java)
                 startActivity(intent)
             }
-            binding.btnPoints.setOnClickListener {
+           btnPoints.setOnClickListener {
                 val intent = Intent(activity, ExchangeTokenActivity::class.java)
                 startActivity(intent)
+            }
+            btnLogout.setOnClickListener {
+                showLogoutDialog()
             }
             val features = listOf(
                btnSubscription,
                 btnSharetofriend, btnPrivacy,
-                btnLanguage, btnDarkmode, btnLogout
+                btnLanguage, btnDarkmode
             )
 
             features.forEach { btn ->
@@ -63,6 +74,29 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         }
     }
 
+    private fun showLogoutDialog() {
+        val customDialogView = LayoutInflater.from(requireContext()).inflate(R.layout.alert_dialog_logout, null)
+        val alertDialog = buildAlertDialog(customDialogView)
+        val yesButton = customDialogView.findViewById<Button>(R.id.btnyes)
+        val noButton = customDialogView.findViewById<Button>(R.id.btnno)
+        yesButton.setOnClickListener {
+            handleYesButtonClick()
+        }
+        noButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+        alertDialog.show()
+    }
+    private fun buildAlertDialog(customDialogView: View): AlertDialog {
+        return AlertDialog.Builder(requireContext())
+            .setView(customDialogView)
+            .create()
+    }
+    private fun handleYesButtonClick() {
+        loginViewModel.deleteUserLogin()
+        startActivity(Intent(requireContext(), LoginActivity::class.java))
+        requireActivity().finish()
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
