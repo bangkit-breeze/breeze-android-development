0b584ab2a75395393a6b476921f182d1346923f2
pierreviences <imaniyazid@gmail.com>
2023-12-08 18:47:13 +0700

    feat: add logic token splash

--- app/src/main/java/com/example/breeze/ui/fragments/splash/SplashFragment.kt
+++ app/src/main/java/com/example/breeze/ui/fragments/splash/SplashFragment.kt
@@ -1,5 +1,6 @@
 package com.example.breeze.ui.fragments.splash
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -11,6 +12,10 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.breeze.R
+import com.example.breeze.ui.activities.login.LoginActivity
+import com.example.breeze.ui.activities.login.LoginViewModel
+import com.example.breeze.ui.activities.main.MainActivity
+import com.example.breeze.ui.factory.AuthViewModelFactory
 import com.example.breeze.ui.fragments.onboarding.OnBoardingViewModel
 import com.example.breeze.ui.fragments.onboarding.OnBoardingViewModelFactory
 import com.example.breeze.utils.Constants
@@ -20,15 +25,35 @@ class SplashFragment : Fragment() {
     private val viewModel: OnBoardingViewModel by viewModels {
         OnBoardingViewModelFactory(requireContext())
     }
+
+    private val loginViewModel: LoginViewModel by viewModels {
+        AuthViewModelFactory.getInstance(requireActivity().application)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Handler(Looper.getMainLooper()).postDelayed({
-            lifecycleScope.launch {
-                findNavController().navigate(getDestination())
-            }
-        }, Constants.SPLASH_SCREEN_DELAY)
+        loginViewModel.getUserLogin().observe(viewLifecycleOwner) { user ->
+            val delayIntent = Intent(
+                requireActivity(),
+                if (user.token.isNullOrEmpty()) {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        lifecycleScope.launch {
+                            findNavController().navigate(getDestination())
+                        }
+                    }, Constants.SPLASH_SCREEN_DELAY)
+                    return@observe
+                } else {
+                    MainActivity::class.java
+                }
+            )
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(delayIntent)
+                requireActivity().finish()
+            }, Constants.SPLASH_SCREEN_DELAY)
+        }
+
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
     private suspend fun getDestination(): Int {
