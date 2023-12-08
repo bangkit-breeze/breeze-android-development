25399fb644b96d46b3037cd18062071ea4cf7149
pierreviences <imaniyazid@gmail.com>
2023-12-08 23:36:40 +0700

    add: Add article response

---
+++ app/src/main/java/com/example/breeze/data/model/ArticleResponse.kt
@@ -0,0 +1,39 @@
+package com.example.breeze.data.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+data class ArticleResponse(
+
+	@field:SerializedName("data")
+	val data: List<DataArticle?>? = null,
+
+	@field:SerializedName("success")
+	val success: Boolean? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
+
+@Parcelize
+data class DataArticle(
+
+	@field:SerializedName("image_url")
+	val imageUrl: String? = null,
+
+	@field:SerializedName("created_at")
+	val createdAt: String? = null,
+
+	@field:SerializedName("description")
+	val description: String? = null,
+
+	@field:SerializedName("content_url")
+	val contentUrl: String? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null
+): Parcelable
