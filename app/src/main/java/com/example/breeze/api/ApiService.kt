4405936746ea8cb7a15a85fa1e14532298d34cc4
pierreviences <imaniyazid@gmail.com>
2023-12-08 23:45:23 +0700

    feat: Implement live data for fetching articles in repository

--- app/src/main/java/com/example/breeze/api/ApiService.kt
+++ app/src/main/java/com/example/breeze/api/ApiService.kt
@@ -1,11 +1,15 @@
 package com.example.breeze.api
 
+import com.example.breeze.data.model.ArticleResponse
+import com.example.breeze.data.model.DataArticle
 import com.example.breeze.data.model.auth.LoginRequest
 import com.example.breeze.data.model.auth.LoginResponse
 import com.example.breeze.data.model.auth.RegisterResponse
 import com.example.breeze.data.model.auth.RegisterRequest
 import retrofit2.http.Body
 import retrofit2.http.Field
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
@@ -18,4 +22,9 @@ interface ApiService {
     @Headers("Content-Type: application/json")
     @POST("login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
+
+    @GET("articles")
+    suspend fun getArticles(
+        @Header("Authorization") token: String
+    ): ArticleResponse
 }
---
+++ app/src/main/java/com/example/breeze/data/repository/ArticleRepository.kt
@@ -0,0 +1,54 @@
+package com.example.breeze.data.repository
+
+import android.app.Application
+import androidx.lifecycle.liveData
+import com.example.breeze.R
+import com.example.breeze.api.ApiService
+import com.example.breeze.data.model.ErrorResponse
+import retrofit2.HttpException
+import java.io.IOException
+import com.example.breeze.utils.Result
+import com.google.gson.Gson
+
+class ArticleRepository private constructor(
+    private val apiService: ApiService,
+    private val application: Application
+) {
+
+    fun getStories(token: String) = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.getArticles("Bearer $token")
+            emit(Result.Success(response))
+        }catch (e: HttpException) {
+            emit(handleHttpException(e))
+        } catch (exception: IOException) {
+            emit(Result.Error(application.resources.getString(R.string.network_error_message)))
+        } catch (exception: Exception) {
+            emit(Result.Error(exception.message ?: application.resources.getString(R.string.unknown_error)))
+        }
+    }
+
+    private fun handleHttpException(exception: HttpException): Result.Error {
+        val jsonInString = exception.response()?.errorBody()?.string()
+        val errorBody = Gson().fromJson(jsonInString, ErrorResponse::class.java)
+        val errorMessage = errorBody.message
+        return Result.Error(errorMessage)
+    }
+
+    private fun getAuthToken(token: String): String {
+        return "Bearer $token"
+    }
+
+    companion object {
+        @Volatile
+        private var instance: ArticleRepository? = null
+        fun getInstance(
+            apiService: ApiService,
+            application: Application
+        ): ArticleRepository =
+            instance ?: synchronized(this) {
+                instance ?: ArticleRepository(apiService, application)
+            }.also { instance = it }
+    }
+}
