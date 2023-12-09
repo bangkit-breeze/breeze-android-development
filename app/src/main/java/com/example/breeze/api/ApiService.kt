730bf9d857150ddad5f83d2fbc845d660151aae8
pierreviences <imaniyazid@gmail.com>
2023-12-09 21:04:15 +0700

    feat: add feature for uploading event evidence

--- app/src/main/java/com/example/breeze/api/ApiService.kt
+++ app/src/main/java/com/example/breeze/api/ApiService.kt
@@ -8,12 +8,17 @@ import com.example.breeze.data.model.auth.RegisterResponse
 import com.example.breeze.data.model.auth.RegisterRequest
 import com.example.breeze.data.model.event.EventResponse
 import com.example.breeze.data.model.event.JoinEventResponse
+import com.example.breeze.data.model.event.UploadEvidenceEventResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -54,4 +59,13 @@ interface ApiService {
         @Header("Authorization") token: String,
         @Path("id") id: String
     ): JoinEventResponse
+
+    @Multipart
+    @POST("events/{id}/upload-evidence")
+    suspend fun uploadEvidenceEvent(
+        @Header("Authorization") token: String,
+        @Path("id") id: String,
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody
+    ): UploadEvidenceEventResponse
 }
