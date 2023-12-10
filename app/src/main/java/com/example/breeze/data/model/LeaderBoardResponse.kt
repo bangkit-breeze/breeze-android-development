a9d5038eb0a492e940cd10a2dba57ed386748288
pierreviences <imaniyazid@gmail.com>
2023-12-10 10:34:58 +0700

    feat: Add LeaderBoardResponse data class

---
+++ app/src/main/java/com/example/breeze/data/model/LeaderBoardResponse.kt
@@ -0,0 +1,39 @@
+package com.example.breeze.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class LeaderBoardResponse(
+
+	@field:SerializedName("data")
+	val dataLeaderboard: List<Data>? = null,
+
+	@field:SerializedName("success")
+	val success: Boolean? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
+
+data class Data(
+
+	@field:SerializedName("leaderboard")
+	val leaderboard: List<LeaderboardItem?>? = null,
+
+	@field:SerializedName("userRank")
+	val userRank: Int? = null
+)
+
+data class LeaderboardItem(
+
+	@field:SerializedName("full_name")
+	val fullName: String? = null,
+
+	@field:SerializedName("user_id")
+	val userId: String? = null,
+
+	@field:SerializedName("rank")
+	val rank: Int? = null,
+
+	@field:SerializedName("points")
+	val points: Int? = null
+)
