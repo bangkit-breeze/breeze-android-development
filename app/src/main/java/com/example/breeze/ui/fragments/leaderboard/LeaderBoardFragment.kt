815ba21ca5feadb7eb965a60cca5f551ddd69eb9
pierreviences <imaniyazid@gmail.com>
2023-12-10 10:55:49 +0700

    add: add leaderboard view model

--- app/src/main/java/com/example/breeze/ui/fragments/leaderboard/LeaderBoardFragment.kt
+++ app/src/main/java/com/example/breeze/ui/fragments/leaderboard/LeaderBoardFragment.kt
@@ -5,23 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.example.breeze.R
+import com.example.breeze.data.model.auth.LoginResult
 import com.example.breeze.databinding.FragmentEventBinding
 import com.example.breeze.databinding.FragmentLeaderBoardBinding
 import com.example.breeze.ui.adapter.EventPagerAdapter
+import com.example.breeze.ui.factory.LeaderBoardViewModelFactory
+import com.example.breeze.ui.factory.ViewModelFactory
 import com.example.breeze.ui.fragments.event.screen.ActiveEventFragment
 import com.example.breeze.ui.fragments.event.screen.ExploreEventFragment
 import com.example.breeze.ui.fragments.event.screen.FinishedEventFragment
+import com.example.breeze.ui.fragments.home.HomeViewModel
 import com.example.breeze.ui.fragments.leaderboard.screen.AllLeaderBoardFragment
 import com.example.breeze.ui.fragments.leaderboard.screen.WeekLeaderBoardFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-//
+
 class LeaderBoardFragment : Fragment() {
     private var _binding: FragmentLeaderBoardBinding? = null
     private val binding get() = _binding!!
+    private val  viewModel: LeaderBoardViewModel by viewModels {
+        LeaderBoardViewModelFactory.getInstance(requireActivity().application)
+    }
+    private lateinit var dataUser: LoginResult
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
---
+++ app/src/main/java/com/example/breeze/ui/fragments/leaderboard/LeaderBoardViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.breeze.ui.fragments.leaderboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.breeze.data.model.auth.LoginResult
+import com.example.breeze.data.repository.EventRepository
+import com.example.breeze.data.repository.LeaderBoardRepository
+import com.example.breeze.data.repository.UserRepository
+
+class LeaderBoardViewModel(
+    private val userRepository: UserRepository,
+    private val leaderBoardRepository: LeaderBoardRepository
+) : ViewModel() {
+    fun getLeaderBordAllTime(token: String) = leaderBoardRepository.getLeaderBoardAllTime(token)
+    fun getUserLogin(): LiveData<LoginResult> =  userRepository.getSession()
+
+}
