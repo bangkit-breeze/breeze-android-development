41f1473f9e72781d4d07f854dc9a196ed1290e15
pierreviences <imaniyazid@gmail.com>
2023-12-10 13:50:33 +0700

    feat: add track vehicle carbon finished

--- app/src/main/java/com/example/breeze/ui/activities/vehicle/AddVehicleCarbonActivity.kt
+++ app/src/main/java/com/example/breeze/ui/activities/vehicle/AddVehicleCarbonActivity.kt
@@ -1,16 +1,35 @@
 package com.example.breeze.ui.activities.vehicle
 
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import com.example.breeze.R
+import com.example.breeze.data.model.auth.LoginResult
+import com.example.breeze.data.model.event.DataEvent
 import com.example.breeze.databinding.ActivityAddVehicleCarbonBinding
 import com.example.breeze.databinding.ActivityLoginBinding
+import com.example.breeze.ui.activities.details.events.DetailEventActivity
+import com.example.breeze.ui.activities.details.events.DetailEventViewModel
+import com.example.breeze.ui.activities.main.MainActivity
+import com.example.breeze.ui.factory.EventViewModelFactory
+import com.example.breeze.ui.factory.TrackEmissionViewModelFactory
+import com.example.breeze.utils.Result
 import kotlin.math.roundToInt
 
 class AddVehicleCarbonActivity : AppCompatActivity() {
@@ -18,6 +37,12 @@ class AddVehicleCarbonActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddVehicleCarbonBinding
     private var lastSelectedTextView: TextView? = null
     private var selectedVehicle: String? = null
+    private val  viewModel: AddVehicleCarbonViewModel by viewModels {
+        TrackEmissionViewModelFactory.getInstance(application)
+    }
+    private lateinit var dataUser: LoginResult
+    private lateinit var progressDialog: Dialog
+    private lateinit var alertDialog: AlertDialog.Builder
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             binding = ActivityAddVehicleCarbonBinding.inflate(layoutInflater)
@@ -25,6 +50,14 @@ class AddVehicleCarbonActivity : AppCompatActivity() {
             binding.topAppBar.setNavigationOnClickListener {
                 onBackPressed()
             }
+
+            viewModel.getUserLogin().observe(this@AddVehicleCarbonActivity) { result ->
+                result?.let {
+                    dataUser = it
+                }
+            }
+
+            alertDialog = AlertDialog.Builder(this@AddVehicleCarbonActivity)
 
             // selectedVehicle = "car"
             binding.item1.setOnClickListener { handleItemClick(binding.item1, binding.tvVehicle1, "car") }
@@ -36,9 +69,7 @@ class AddVehicleCarbonActivity : AppCompatActivity() {
             // binding.coba.text = selectedVehicle
 
 
-            binding.btnAdd.setOnClickListener {
 
-            }
 
             binding.mainSlider.setLabelFormatter { value ->
                 "${value.toInt()} KM"
@@ -66,13 +97,79 @@ class AddVehicleCarbonActivity : AppCompatActivity() {
                         binding.etMileage.setText("200")
                     }else {
                         if (binding.etMileage.isFocused) {
-                            binding.etMileage.setSelection(binding.etMileage.length())  // Move cursor to the end
+                            binding.etMileage.setSelection(binding.etMileage.length())
                         }
                         binding.mainSlider.value = enteredValue.toFloat()
                     }
                 }
             })
+
+            binding.btnAdd.setOnClickListener {
+                val mileageValue = binding.etMileage.text.toString()
+                if (mileageValue.isEmpty()) {
+                    showToast("Tolong isi jarak kendaraan!")
+                }
+                if (selectedVehicle?.isNotEmpty() == true) {
+                    viewModel.addTrackEmissionVehicleCarbon(dataUser.token, selectedVehicle!!, mileageValue.toInt()).observe(this@AddVehicleCarbonActivity) { result ->
+                        when (result) {
+                            is Result.Loading -> showProgressDialog()
+                            is Result.Success -> {
+                                hideProgressDialog()
+                                showSuccessDialog()
+                                Handler(Looper.getMainLooper()).postDelayed({
+                                    val intent = Intent(this, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }, 3000)
+                            }
+                            is Result.Error -> onLoginError(result.error)
+                        }
+                    }
+                } else {
+                    showToast("Please select a vehicle first!")
+                }
+
+
+
+            }
         }
+
+    private fun showProgressDialog() {
+        progressDialog = Dialog(this)
+        progressDialog.setContentView(R.layout.custom_progressbar)
+        val progressBar: ProgressBar = progressDialog.findViewById(R.id.progressBar)
+        progressBar.isIndeterminate = true
+        progressDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        progressDialog.show()
+    }
+    private fun hideProgressDialog() {
+        if (::progressDialog.isInitialized && progressDialog.isShowing) {
+            progressDialog.dismiss()
+        }
+    }
+
+    private fun showSuccessDialog() {
+        val customDialogView = LayoutInflater.from(this).inflate(R.layout.alert_dialog_success, null)
+        val alertDialog = android.app.AlertDialog.Builder(this)
+            .setView(customDialogView)
+            .create()
+        alertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        alertDialog.show()
+
+        val tvDescription: TextView = customDialogView.findViewById(R.id.tv_description)
+        tvDescription.text = "Anda telah menambahkan track vehcle carbon, anda dapat 10xp"
+        Handler(Looper.getMainLooper()).postDelayed({
+            alertDialog.dismiss()
+        }, 3000)
+    }
+    private fun onLoginError(errorMessage: String) {
+        hideProgressDialog()
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 
     private fun handleItemClick(item: LinearLayout, textView: TextView, vehicleType: String) {
         if (selectedItemId == item.id) {
@@ -125,7 +222,7 @@ class AddVehicleCarbonActivity : AppCompatActivity() {
     private fun resetItemSelection(item: LinearLayout, textView: TextView?) {
         item.isSelected = false
         item.setBackgroundResource(R.drawable.bg_vehicle)
-        textView?.setTextColor(resources.getColor(R.color.black))
+        textView?.setTextColor(0xFF858585.toInt())
 
         when (item.id) {
             R.id.item1 -> {
