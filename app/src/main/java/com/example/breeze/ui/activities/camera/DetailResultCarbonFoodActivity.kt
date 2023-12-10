0b9c23776b0a076d347c9b6e0ce492ae25ab2eef
pierreviences <imaniyazid@gmail.com>
2023-12-10 19:00:20 +0700

    feat: display detailed carbon footprint results for tracked food

--- app/src/main/java/com/example/breeze/ui/activities/camera/DetailResultCarbonFoodActivity.kt
+++ app/src/main/java/com/example/breeze/ui/activities/camera/DetailResultCarbonFoodActivity.kt
@@ -2,13 +2,19 @@ package com.example.breeze.ui.activities.camera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.breeze.R
 import com.example.breeze.data.model.DataTrackFood
 import com.example.breeze.databinding.ActivityDetailResultCarbonFoodBinding
 import com.example.breeze.databinding.ActivityResultCameraFoodBinding
+import com.example.breeze.ui.adapter.FoodCarbonAdapter
+import com.example.breeze.ui.adapter.LeaderBoardAdapter
+import kotlin.math.roundToInt
 
 class DetailResultCarbonFoodActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailResultCarbonFoodBinding
+    private val adapter = FoodCarbonAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailResultCarbonFoodBinding.inflate(layoutInflater)
@@ -17,6 +23,27 @@ class DetailResultCarbonFoodActivity : AppCompatActivity() {
         if (data != null) {
             val foodName = data.predictResult?.foodName
             binding.tvTitle.text = foodName
+            Glide.with(this)
+                .load(data.imageUrl)
+                .into(binding.myImageView)
+            val acuracy = data.predictResult?.confidence?.toFloat()
+            if (acuracy != null) {
+                if(acuracy < 0.6 && acuracy > 0.5){
+                    binding.tvAccuacy.text = "Low Accuracy"
+                }else if(acuracy < 0.8 && acuracy > 0.7){
+                    binding.tvAccuacy.text = "Medium Accuracy"
+                }else if(acuracy > 0.9){
+                    binding.tvAccuacy.text = "High Accuracy"
+                }
+            }
+
+            val emision = data.predictResult?.totalEmissions?.toFloat()
+            val totalEmision = emision?.let { (it * 1000).roundToInt() } ?: 0
+            binding.tvCarbon.text = "${data.predictResult?.totalEmissions} kgCO2e"
+            val layoutManager = LinearLayoutManager(this)
+            binding.rvIngredients.layoutManager = layoutManager
+            binding.rvIngredients.adapter = FoodCarbonAdapter()
+            FoodCarbonAdapter().submitList(data.predictResult?.ingredients)
         }
 
     }
---
+++ app/src/main/java/com/example/breeze/ui/adapter/FoodCarbonAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.breeze.ui.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.breeze.R
+import com.example.breeze.data.model.IngredientsItem
+import com.example.breeze.data.model.event.DataEvent
+import com.example.breeze.databinding.ItemCarbonBinding
+import com.example.breeze.databinding.ItemEventBinding
+import com.example.breeze.databinding.ItemIngredientsBinding
+import com.example.breeze.ui.activities.details.events.DetailEventActivity
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class FoodCarbonAdapter: ListAdapter<IngredientsItem, FoodCarbonAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    class MyViewHolder(private val binding: ItemIngredientsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: IngredientsItem) = with(binding) {
+            val berat = ((data.berat?.toFloat() ?: 0f) * 1000)
+            tvTitle.text = "$berat g ${data.bahan}"
+            tvEmisi.text = "${data.carbonFootprint} kgCO2e"
+
+        }
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder =
+        MyViewHolder(
+            ItemIngredientsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<IngredientsItem>() {
+            override fun areItemsTheSame(oldItem: IngredientsItem, newItem: IngredientsItem): Boolean {
+                return oldItem == newItem
+            }
+            override fun areContentsTheSame(oldItem: IngredientsItem, newItem: IngredientsItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
