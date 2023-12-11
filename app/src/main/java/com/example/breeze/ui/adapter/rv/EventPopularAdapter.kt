0aa7343372fe455ee7a6e147bccbb81b1abbb4ec
pierreviences <imaniyazid@gmail.com>
2023-12-11 17:55:52 +0700

    feat: add adapter popular event

---
+++ app/src/main/java/com/example/breeze/ui/adapter/rv/EventPopularAdapter.kt
@@ -0,0 +1,71 @@
+package com.example.breeze.ui.adapter.rv
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.breeze.R
+import com.example.breeze.data.model.response.event.DataEvent
+import com.example.breeze.data.model.response.event.DataEventPopular
+import com.example.breeze.databinding.ItemEventBinding
+import com.example.breeze.databinding.ItemEventPopulerBinding
+import com.example.breeze.ui.activities.details.events.DetailEventActivity
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class EventPopularAdapter: ListAdapter<DataEventPopular, EventPopularAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    class MyViewHolder(private val binding: ItemEventPopulerBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: DataEventPopular) = with(binding) {
+            tvTitle.text = data.name
+            tvDesc.text = data.description
+            tvPoint.text = data.rewardPoints.toString()
+            tvLocation.text = data.location
+
+
+            Glide.with(itemView.context)
+                .load(data.eventImageUrl)
+                .placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+                .fallback(R.drawable.ic_launcher_foreground)
+                .into(ivEvent)
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, DetailEventActivity::class.java).apply {
+                    putExtra(DetailEventActivity.STORY_INTENT_DATA, data)
+                }
+                itemView.context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder =
+        MyViewHolder(
+            ItemEventPopulerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<DataEventPopular>() {
+            override fun areItemsTheSame(oldItem: DataEventPopular, newItem: DataEventPopular): Boolean {
+                return oldItem == newItem
+            }
+            override fun areContentsTheSame(oldItem: DataEventPopular, newItem: DataEventPopular): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
