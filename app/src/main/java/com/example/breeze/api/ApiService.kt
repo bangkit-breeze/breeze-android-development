c15e664f225efed995a23616264b5b7d0986df98
pierreviences <imaniyazid@gmail.com>
2023-12-11 17:52:19 +0700

    add: add api & repository event popular

--- app/src/main/java/com/example/breeze/api/ApiService.kt
+++ app/src/main/java/com/example/breeze/api/ApiService.kt
@@ -12,6 +12,7 @@ import com.example.breeze.data.model.request.LoginRequest
 import com.example.breeze.data.model.response.auth.LoginResponse
 import com.example.breeze.data.model.response.auth.RegisterResponse
 import com.example.breeze.data.model.request.RegisterRequest
+import com.example.breeze.data.model.response.event.EventPopularResponse
 import com.example.breeze.data.model.response.event.EventResponse
 import com.example.breeze.data.model.response.event.JoinEventResponse
 import com.example.breeze.data.model.response.event.UploadEvidenceEventResponse
@@ -58,6 +59,11 @@ interface ApiService {
         @Header("Authorization") token: String,
         @Query("status") query: String = "finished"
     ): EventResponse
+
+    @GET("events/popular")
+    suspend fun getEventsPopular(
+        @Header("Authorization") token: String
+    ): EventPopularResponse
 
     @POST("events/{id}/join")
     suspend fun joinEvent(
--- app/src/main/java/com/example/breeze/data/repository/EventRepository.kt
+++ app/src/main/java/com/example/breeze/data/repository/EventRepository.kt
@@ -63,6 +63,20 @@ class EventRepository private constructor(
         }
     }
 
+    fun getEventPopular(token: String) = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.getEventsPopular("Bearer $token")
+            emit(Result.Success(response))
+        }catch (e: HttpException) {
+            emit(handleHttpException(e))
+        } catch (exception: IOException) {
+            emit(Result.Error(application.resources.getString(R.string.network_error_message)))
+        } catch (exception: Exception) {
+            emit(Result.Error(exception.message ?: application.resources.getString(R.string.unknown_error)))
+        }
+    }
+
     fun joinEvent(token: String, id: String) = liveData {
         emit(Result.Loading)
         try {
