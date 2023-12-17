78b5a0e7727cf944a47d76c3793aa0b6273eadfa
pierreviences <imaniyazid@gmail.com>
2023-12-17 17:46:54 +0700

    feat: add detail project offset

--- app/src/main/java/com/example/breeze/ui/activities/project/DetailProjectActivity.kt
+++ app/src/main/java/com/example/breeze/ui/activities/project/DetailProjectActivity.kt
@@ -2,7 +2,9 @@ package com.example.breeze.ui.activities.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.breeze.R
+import com.example.breeze.data.model.response.project.DataProject
 import com.example.breeze.databinding.ActivityDetailProjectBinding
 import com.example.breeze.databinding.ActivityProjectBinding
 
@@ -14,6 +16,27 @@ class DetailProjectActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.icBack.setOnClickListener {
             onBackPressed()
+        }
+        val dataProject: DataProject? = intent.getParcelableExtra(STORY_INTENT_DATA)
+
+        dataProject?.let { displayProjectDetails(it) }
+    }
+    private fun displayProjectDetails(dataProject: DataProject) {
+        with(binding) {
+            Glide.with(this@DetailProjectActivity)
+                .load(dataProject.imageUrl)
+                .placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+                .fallback(R.drawable.ic_launcher_foreground)
+                .into(ivEvent)
+
+            tvTitle.text = dataProject.title
+            tvLocation.text = dataProject.location
+            tvCarbon.text = "${dataProject.totalCo2?.div(1000)}"
+            tvHarga.text = "Rp. ${dataProject.price}"
+            tvTree.text = "/ Tree"
+            tvDesc.text = dataProject.description
+            tvBennefit.text = "${dataProject.benefits}"
         }
     }
     companion object {
