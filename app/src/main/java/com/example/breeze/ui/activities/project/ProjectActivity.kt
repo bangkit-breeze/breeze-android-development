d46c941d66475d955a2e19ee89c580abb505df0c
pierreviences <imaniyazid@gmail.com>
2023-12-17 15:30:03 +0700

    style(ui): add detail and list project

---
+++ app/src/main/java/com/example/breeze/ui/activities/project/ProjectActivity.kt
@@ -0,0 +1,12 @@
+package com.example.breeze.ui.activities.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.breeze.R
+
+class ProjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project)
+    }
+}
